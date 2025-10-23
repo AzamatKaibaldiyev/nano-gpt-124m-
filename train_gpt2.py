1f10e4884db023ac9373f05bdd6cf2fd4e94a286
azamatkaibaldiyev <azamat.kaibaldiyev@alumni.nu.edu.kz>
2025-10-23 16:22:22 +0200

    added reset for dataloader

--- train_gpt2.py
+++ train_gpt2.py
@@ -239,11 +239,11 @@ class DataLoaderLite:
                print(f"found {len(shards)} shards for split {split}")
           self.reset()
 
-          def reset(self):
-               # state. init at zero shard
-               self.current_shard = 0
-               self.tokens = load_tokens(self.shards[self.current_shard])
-               self.current_position = self.B*self.T*self.process_rank
+     def reset(self):
+          # state. init at zero shard
+          self.current_shard = 0
+          self.tokens = load_tokens(self.shards[self.current_shard])
+          self.current_position = self.B*self.T*self.process_rank
 
 
 
